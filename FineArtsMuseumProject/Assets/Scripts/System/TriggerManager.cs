c2ce0c205cf1355a2896a46c3f628a8ab2cafbd5
ngocthanh-shu <hongocthanh1712@gmail.com>
2025-04-22 14:36:39 +0700

    update script

--- FineArtsMuseumProject/Assets/Scripts/System/TriggerManager.cs
+++ FineArtsMuseumProject/Assets/Scripts/System/TriggerManager.cs
@@ -7,6 +7,7 @@ namespace System
     public class TriggerManager : MonoSingleton<TriggerManager>
     {
         [field: SerializeField] private List<BoxCollider> TriggerColliders { get; set; }
+        [field: SerializeField] private List<BoxCollider> AllTriggerColliders { get; set; }
 
         public void EnableTriggerColliders()
         {
@@ -22,6 +23,26 @@ namespace System
         {
             if (TriggerColliders == null || TriggerColliders.Count == 0) return;
             foreach (var triggerCollider in TriggerColliders)
+            {
+                triggerCollider.enabled = false;
+                //triggerCollider.gameObject.SetActive(false);
+            }
+        }
+        
+        public void EnableAllTriggerColliders()
+        {
+            if (AllTriggerColliders == null || AllTriggerColliders.Count == 0) return;
+            foreach (var triggerCollider in AllTriggerColliders)
+            {
+                triggerCollider.enabled = true;
+                //triggerCollider.gameObject.SetActive(true);
+            }
+        }
+        
+        public void DisableAllTriggerColliders()
+        {
+            if (AllTriggerColliders == null || AllTriggerColliders.Count == 0) return;
+            foreach (var triggerCollider in AllTriggerColliders)
             {
                 triggerCollider.enabled = false;
                 //triggerCollider.gameObject.SetActive(false);
