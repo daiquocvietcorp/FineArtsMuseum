a7a4c8710ebe0d36bcc32f5a791859780167db8f
Nguyen Huynh Minh Khanh <elboywithgear@gmail.com>
2026-01-07 18:18:28 +0700

    Fork

--- FineArtsMuseumProject/Assets/Scripts/SceneLog.cs
+++ FineArtsMuseumProject/Assets/Scripts/SceneLog.cs
@@ -10,7 +10,7 @@ public class SceneLog : MonoBehaviour
     public static bool IsShowedGuideObject = false;
     public static bool IsShowedGuideImage = false;
     public static bool IsVietnamese = true;
-    public static bool IsFirstView = true;
+    public static bool IsFirstView = false;
     public static bool IsNewController = false;
     public static bool IsBlockFirstView = false;
     public static bool IsBlockThirdView = false;
